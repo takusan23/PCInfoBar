6315db73f083bbb4ce5aac9fd5d102912f033293
takusan23 <takumu1200@gmail.com>
2020-01-18 13:18:13 +0900

    拡張うごかない

---
+++ pcinfobar/src/extension.ts
@@ -0,0 +1,79 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { setInterval } from 'timers';
+import { userInfo, totalmem } from 'os';
+import { stat } from 'fs';
+
+// CPU使用率取得など
+const osu = require('node-os-utils');
+const systemInfomation = require('systeminformation');
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+export function activate(context: vscode.ExtensionContext) {
+
+	console.log('Congratulations, your extension "pcinfobar" is now active!');
+
+	// 起動コマンド
+	const command = "extension.pcinfo";
+	// 更新周期
+	const interval = 1000;
+
+
+	let disposable = vscode.commands.registerCommand(command, () => {
+		vscode.window.showInformationMessage('起動するね!');
+	});
+	context.subscriptions.push(disposable);
+
+	let batteryLevel = '';
+	let clockSpeed = '';
+	let cpuUsage = '';
+	let cpuName = '';
+	let ramUsage = '';
+	let ramTotal = '';
+
+	// ステータスバー追加
+	// CPU 使用率
+	const statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	context.subscriptions.push(statusBar);
+	// CPU 速度など
+	setInterval(() => {
+		systemInfomation.cpu().then((data: any) => {
+			cpuName = data.brand;
+		});		
+		systemInfomation.cpuCurrentspeed().then((speed: any) => {
+			clockSpeed = speed.avg;
+		});	
+		osu.cpu.usage().then((cpuPercentage: any) => {
+			cpuUsage = cpuPercentage;
+		});
+	}, interval);
+
+	// RAM
+	setInterval(() => {
+		systemInfomation.mem().then((ram: any) => {
+			ramUsage = (Number(ram.used) / 1024 / 1024 / 1024).toFixed(2);
+			ramTotal = (Number(ram.total) / 1024 / 1024 / 1024).toFixed(2);
+		});
+	}, interval);
+
+	// Battery
+	setInterval(() => {
+		systemInfomation.battery().then((battery: any) => {
+			batteryLevel = battery.percent;
+		});
+	}, interval);
+
+	// 表示
+	setInterval(() => {
+		statusBar.text = `$(dashboard) ${clockSpeed} GHz $(pulse) ${cpuUsage} % $(package) ${ramUsage} GB / ${ramTotal} GB $(plug) ${batteryLevel} %`;
+		statusBar.tooltip = 'CPU平均速度 / CPU使用率 / メモリ使用量 (メモリ搭載量) / 電池残量';
+		statusBar.show();
+	}, interval);
+
+
+}
+
+export function deactivate() { }
